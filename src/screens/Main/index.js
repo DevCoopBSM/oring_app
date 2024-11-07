072f203beb0804d0f740e3897ba5622385a3fa1e
E-jiwoo <livei07@naver.com>
2024-11-07 17:50:33 +0900

    .

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, styled} from 'react-native';
 
 const Main = () => {
   return (
-    <View style={styles.container}>
+    <View style={styled.container}>
       <Text>메인 화면</Text>
     </View>
   );
