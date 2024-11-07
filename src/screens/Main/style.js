76a956cee156f7e53ea367dd533463d710f0ae63
E-jiwoo <livei07@naver.com>
2024-11-07 19:17:15 +0900

    init :: 초기화

--- src/screens/Main/style.js
+++
@@ -1,9 +0,0 @@
-import styled, {css} from 'styled-components/native';
-
-export const styles = styled.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
