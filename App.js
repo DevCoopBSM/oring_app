d34b4f6b302d9c4aae7745c77d36850f8a8dbfa2
E-jiwoo <livei07@naver.com>
2024-11-12 20:33:34 +0900

    add :: User_Chat - 유저 채팅화면

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Splash from './src/screens/Splash';
 import Main from './src/screens/Main';
 import Item_List from './src/screens/Items_List';
+import User_Chat from './src/screens/User_Chat';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,7 @@ const App = () => {
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Item_List" component={Item_List} />
+        <Stack.Screen name="User_Chat" component={User_Chat} />
       </Stack.Navigator>
     </NavigationContainer>
   );
